5eacf1bcb0f7344d0027e25e50523833971a4ccb
kein <kein10119810@gmail.com>
2025-06-04 18:47:30 +0900

    アカウント削除画面_データの取得

--- delete.php
+++ delete.php
@@ -14,6 +14,8 @@ $stmt = $pdo->prepare("
 ");
 $stmt->execute([$id]);
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+$maskedPassword = str_repeat("●", 6);
 ?>
 
 <!DOCTYPE html>
@@ -25,6 +27,17 @@ $user = $stmt->fetch(PDO::FETCH_ASSOC);
     <body>
         <h1>アカウント削除画面</h1>
         <p>名前（姓）　<?= htmlspecialchars($user['family_name'], ENT_QUOTES, 'UTF-8') ?></p>
+        <p>名前（名）　<?= htmlspecialchars($user['last_name'], ENT_QUOTES, 'UTF-8') ?></p>
+        <p>カナ（姓）　<?= htmlspecialchars($user['family_name_kana'], ENT_QUOTES, 'UTF-8') ?></p>
+        <p>カナ（名）　<?= htmlspecialchars($user['last_name_kana'], ENT_QUOTES, 'UTF-8') ?></p>
+        <p>メールアドレス　<?= htmlspecialchars($user['mail'], ENT_QUOTES, 'UTF-8') ?></p>
+        <p>パスワード　<?= $maskedPassword ?></p>
+        <p>性別　<?= $user['gender'] == "0" ? "男" : "女" ?></p>
+        <p>郵便番号　<?= htmlspecialchars($user['postal_code'], ENT_QUOTES, 'UTF-8') ?></p>
+        <p>住所（都道府県）　<?= htmlspecialchars($user['prefecture'], ENT_QUOTES, 'UTF-8') ?></p>
+        <p>住所（市区町村）　<?= htmlspecialchars($user['address_1'], ENT_QUOTES, 'UTF-8') ?></p>
+        <p>住所（番地）　<?= htmlspecialchars($user['address_2'], ENT_QUOTES, 'UTF-8') ?></p>
+        <p>アカウント権限　<?= $user['authority'] == "0" ? "一般" : "管理者" ?></p>
     </body>
 
 
